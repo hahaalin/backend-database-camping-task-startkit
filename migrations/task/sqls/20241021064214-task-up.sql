44395c784d3093e7e19d2eab1c01fb7bd1e70a38
Shelly <shelly@adpost.com.tw>
2024-12-04 23:36:00 +0800

    update: add 挑戰題

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -278,6 +278,27 @@ GROUP BY user_id;
     -- inner join ( 用戶王小明的已使用堂數) as "COURSE_BOOKING"
     -- on "COURSE_BOOKING".user_id = "CREDIT_PURCHASE".user_id;
 
+SELECT
+"CREDIT_PURCHASE".user_id,
+("CREDIT_PURCHASE".total_credit - "COURSE_BOOKING".used_credit) as remaining_credit
+FROM
+(SELECT 
+user_id,
+sum(purchased_credits) AS total_credit
+FROM "CREDIT_PURCHASE"
+WHERE user_id = (SELECT id FROM "USER" WHERE email = 'wXlTq@hexschooltest.io')
+GROUP BY user_id) as "CREDIT_PURCHASE"
+INNER JOIN (
+  SELECT 
+user_id,
+COUNT(*) AS used_credit
+FROM "COURSE_BOOKING"
+WHERE user_id = (SELECT id FROM "USER" WHERE email = 'wXlTq@hexschooltest.io')
+AND status != '課程已取消'
+GROUP BY user_id
+) as "COURSE_BOOKING" 
+ON "COURSE_BOOKING".user_id ="CREDIT_PURCHASE".user_id;
+
 
 -- ████████  █████   █     ███  
 --   █ █   ██    █  █     █     
