9ecae87c2ad93898d029bd3bcc6ca492ae33152e
Shelly <shelly@adpost.com.tw>
2024-12-04 23:51:16 +0800

    update:6-4題目分號

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -343,7 +343,7 @@ GROUP BY "CREDIT_PACKAGE".name;
 SELECT 
 SUM(price_paid) AS"總營收"
 FROM "CREDIT_PURCHASE"
-WHERE "CREDIT_PURCHASE".purchase_at >= '2024-12-01 00:00:00' AND "CREDIT_PURCHASE".purchase_at <= '2024-12-31 23:59:59'
+WHERE "CREDIT_PURCHASE".purchase_at >= '2024-12-01 00:00:00' AND "CREDIT_PURCHASE".purchase_at <= '2024-12-31 23:59:59';
 
 -- 6-5. 查詢：計算 11 月份有預約課程的會員人數（需使用 Distinct，並用 created_at 和 status 欄位統計）
 -- 顯示須包含以下欄位： 預約會員人數
